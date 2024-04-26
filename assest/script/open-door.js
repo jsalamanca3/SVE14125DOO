db987631831bae0080672af82e7fbdd6b1595a9a
Julián Fernando Salamanca Chacón <105863909+jsalamanca3@users.noreply.github.com>
2024-04-26 18:20:53 -0500

    cargando javascript

---
+++ assest/script/open-door.js
@@ -0,0 +1,7 @@
+function toggleDoor() {
+  fetch('http://192.168.1.15/toggle_door', {
+    method: 'GET'
+  })
+  .then(response => console.log('Estado de la puerta cambiado correctamente'))
+  .catch(error => console.error('Error al cambiar estado de la puerta:', error));
+}
